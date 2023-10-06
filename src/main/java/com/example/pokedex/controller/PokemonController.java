d548762d81517ab5dec36e6afd1ab5ddd01cab2f
Mateus Souza da Silva <mma.teuzzz@gmail.com>
2023-10-06 18:32:53 -0300

    update PokemonController, pokedex.html and style.css

--- src/main/java/com/example/pokedex/controller/PokemonController.java
+++ src/main/java/com/example/pokedex/controller/PokemonController.java
@@ -1,7 +1,9 @@
 package com.example.pokedex.controller;
 
+import com.example.pokedex.model.Pokemon;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PokemonController {
@@ -9,5 +11,10 @@ public class PokemonController {
     @GetMapping("/")
     public String pokedex() {
         return "pokedex";
+    }
+
+    @PostMapping("/")
+    public void criaPokemon(Pokemon pokemon) {
+        System.out.println("O novo pokemon é: " + pokemon.getNome());
     }
 }
